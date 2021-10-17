9a92a63a3c72f2c90995dbdd0f572ba4a39d0de0
atila-tokyo <atilamanager@gmail.com>
2021-10-17 23:38:32 +0900

    Adding some methods to js script

--- index.js
+++ index.js
@@ -1,5 +1,43 @@
-const dino = document.getElementsByClassName("dino");
+const dino = document.querySelector(".dino");
+const background = document.querySelector(".background");
+
+let isJumping = false;
+
 const handleKeyUp = (event) => {
-    event.keyCode === 32 ? console.log("pressed space") : console.log("else")
+    if (event.keyCode === 32) {
+        if (!isJumping) {
+            jump();   
+        }
+    }
+};
+ 
+const jump = () => {
+    let position = 0;
+
+    isJumping = true;
+    let upInterval = setInterval(() => {
+        if (position >= 150) {
+            clearInterval(upInterval);
+
+            let downInterval = setInterval(() => {
+                if (position <= 0) {
+                    clearInterval(downInterval);
+                    isJumping = false;
+                } else {
+                    position -= 20;
+                    dino.style.bottom = position + 'px';
+                }
+            }, 20);
+        } else {
+            position += 20;
+            dino.style.bottom = position + "px";
+        }
+    }, 20);
+};
+
+const createCactus = () => {
+    const cactus = document.createElement('div');
+    cactus.classList.add('cactus');
+    background.appendChild(cactus);
 };
 document.addEventListener('keyup', handleKeyUp);
