c6f548c19316a379a044fb908ad139f2d37668c8
atila-tokyo <atilamanager@gmail.com>
2021-10-18 22:46:04 +0900

    Adding complete game logic

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const dino = document.querySelector(".dino");
 const background = document.querySelector(".background");
-
+let position = 0;
 let isJumping = false;
 
 const handleKeyUp = (event) => {
@@ -12,7 +12,6 @@ const handleKeyUp = (event) => {
 };
  
 const jump = () => {
-    let position = 0;
 
     isJumping = true;
     let upInterval = setInterval(() => {
@@ -38,6 +37,22 @@ const jump = () => {
 const createCactus = () => {
     const cactus = document.createElement('div');
     let cactusPosition = 1000;
+    let randomTime = Math.random() * 6000;
+
+    let leftInterval = setInterval(() => {
+        if (cactusPosition < -60) {
+            clearInterval(leftInterval);
+            background.removeChild(cactus);
+        } else if (cactusPosition > 0 && cactusPosition < 60 && position < 60) {
+            clearInterval(leftInterval);
+            document.body.innerHTML = '<h1 class="game-over">Game Over</h1>';
+        } else {
+            cactusPosition -= 10;
+            cactus.style.left = cactusPosition + "px";
+        }
+    }, 20);
+
+    setTimeout(createCactus, randomTime);
 
     cactus.classList.add('cactus');
     cactus.style.left = cactusPosition + "px";
